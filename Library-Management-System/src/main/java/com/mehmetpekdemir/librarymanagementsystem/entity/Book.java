dbc8c073b7fa11db1b9dfa7c2abe2cf32371844c
moneeshabodumalla <moneeshabodumalla@192.168.0.3>
2022-09-19 13:45:40 +0530

    commit

---
+++ Library-Management-System/src/main/java/com/mehmetpekdemir/librarymanagementsystem/entity/Book.java
@@ -0,0 +1,62 @@
+package com.mehmetpekdemir.librarymanagementsystem.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "isbn", length = 50, nullable = false, unique = true)
+	private String isbn;
+
+	@Column(name = "name", length = 100, nullable = false)
+	private String name;
+
+	@Column(name = "serialName", length = 50, nullable = false)
+	private String serialName;
+
+	@Column(name = "description", length = 250, nullable = false)
+	private String description;
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE })
+	@JoinTable(name = "books_authors", joinColumns = { @JoinColumn(name = "book_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "author_id") })
+	private Set<Author> authors = new HashSet<Author>();
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	@JoinTable(name = "books_categories", joinColumns = { @JoinColumn(name = "book_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "category_id") })
+	private Set<Category> categories = new HashSet<Category>();
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	@JoinTable(name = "books_publishers", joinColumns = { @JoinColumn(name = "book_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "publisher_id") })
+	private Set<Publisher> publishers = new HashSet<Publisher>();
+
+	public Book(String isbn, String name, String serialName, String description) {
+		this.isbn = isbn;
+		this.name = name;
+		this.serialName = serialName;
+		this.description = description;
+	}
+
+	
+}
